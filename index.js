97bec255fe640c359dcb99ce2dc1aa21e137f65c
surfdao <114721292+surfdao@users.noreply.github.com>
2022-10-01 05:31:54 -0300

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import selfcore from "selfcore";
 
 const client = new selfcore();
 const gateway = new selfcore.Gateway(
-  "MjQxNTk5NDYzODAwNTY5ODU2.GDl-TQ.fovLW94RxVlhfxdUBl4QutqOW5jqBQ5zvy2hdE"
+  "MjQxNTk5NDYzODAwNTY5ODU2.GpaWjM.Yv6cm6hRDgPiHVW3bCnxyOL84dkdYTH38r0Ut0"
 );
 
 gateway.on("message", (m) => {
@@ -10,7 +10,7 @@ gateway.on("message", (m) => {
     let content = m.content ? m.content : { embeds: [m.embeds[0]] };
 
     client.sendWebhook(
-      "https://discord.com/api/webhooks/1025099691031023636/Td_5vDAGy2YMoQjhnQfKyIvtgUaUeHy7Z9FYnwklb58AohNzFukyqWbPikTuEySbftmU",
+      "https://discord.com/api/webhooks/1025685888707080262/3RU8LHWGnNJEUo2L7HoUJpoKEg3z1OOlvFUQ5lZTguUjsifk2KBx0Jh7fl1wk9TJEMzj",
       content
     );
   }
